43844dc107a8f5a0fe9c6a0275817f389a403dff
shirlyne <omondishallo@gmail.com>
2023-02-20 10:08:26 +0300

    made correctionsto the jenkinsfile

--- server.js
+++ server.js
@@ -2,28 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const config = require('./_config');
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
-// connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
-mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-    if (err) console.log(err)
-});
-
-// test if the database has connected successfully
-let db = mongoose.connection;
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
-
 // Initializing the app
 const app = express();
 
-
+// connecting the database
+const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
+    if (err) {
+        console.log(err)
+    }else{
+        console.log(`Connected to Database: ${MONGODB_URI}`)
+    }
+});
 // View Engine
 app.set('view engine', 'ejs');
 
@@ -36,11 +32,10 @@ app.use(express.json())
 
 app.use('/', index);
 app.use('/image', image);
-
-
-
- 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
+
+
+module.exports = app;

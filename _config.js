e203dd4a6dcc945bd1fb227b171c926931f1f051
shirlyne <omondishallo@gmail.com>
2023-02-24 14:04:40 +0300

    render6

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://omondishallo:iQN7HYEAY46YFhGD@cluster0.ywpxquu.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://omondishallo:iQN7HYEAY46YFhGD@cluster0.ywpxquu.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://omondishallo:iQN7HYEAY46YFhGD@cluster0.ywpxquu.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://omondishallo:NEs4ZhsO3F2jqQsN@cluster0.ywpxquu.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://omondishallo:NEs4ZhsO3F2jqQsN@cluster0.ywpxquu.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://omondishallo:NEs4ZhsO3F2jqQsN@cluster0.ywpxquu.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
